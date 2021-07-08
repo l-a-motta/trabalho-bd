3ef6850f5a6c5ea4ce1382a2417dd9db5b5fe618
Leonardo Antonetti da Motta <l.a.motta@usp.br>
2021-07-07 23:36:02 -0300

    add VooAssentos and more
    VooAssentos is added, some comments have been created and tweaked older tables

--- esquema.sql
+++ esquema.sql
@@ -1,8 +1,11 @@
+-- Consideramos que palavras pequenas tem no maximo 30 caracteres, medias 50 e grandes 180 (um tweet) 
+
+-- Criando tabela Destino
 CREATE TABLE Destino (
 	
 	Pais VARCHAR(30),
 	Cidade VARCHAR(30),
-	Descricao TEXT,
+	Descricao VARCHAR(180),
 	Idioma VARCHAR(30) NOT NULL,
 	Clima VARCHAR(30),
 	
@@ -10,6 +13,7 @@ CREATE TABLE Destino (
 	
 )
 
+-- Criando tabela DestinoTags
 CREATE TABLE DestinoTags (
 	
 	Pais VARCHAR(30),
@@ -18,7 +22,7 @@ CREATE TABLE DestinoTags (
 	
 	CONSTRAINT PK_DestinoTag PRIMARY KEY(Pais, Cidade, Tag),
 	CONSTRAINT FK_DestinoTag FOREIGN KEY (Pais, Cidade) REFERENCES Destino(Pais, Cidade) ON DELETE CASCADE ON UPDATE CASCADE
-	
+	-- Se o destino for deletado, nao faz sentido manter as associações das tags dele
 )
 
 CREATE TABLE Aeroporto (
@@ -29,28 +33,46 @@ CREATE TABLE Aeroporto (
 	Bairro VARCHAR(30) NOT NULL,
 	Rua VARCHAR(30) NOT NULL,
 	Numero VARCHAR(30) NOT NULL,
-	CEP VARCHAR(30) NOT NULL,
+	CEP CHAR(9) NOT NULL,-- Um CEP tem no maximo nove caracteres (00000-000)
 	Nome VARCHAR(30) NOT NULL,
 	
 	CONSTRAINT PK_Aeroporto PRIMARY KEY(ID),
 	CONSTRAINT UC_Aeroporto UNIQUE(Pais, Cidade, Bairro, Rua, Numero),
+	-- A chave secundária vale para diferenciarmos os locais dos aeroportos, uma vez que não existem dois aeroportos no mesmo exato local
 	CONSTRAINT FK_AeroportoDestino FOREIGN KEY (Pais, Cidade) REFERENCES Destino(Pais, Cidade) ON DELETE CASCADE ON UPDATE CASCADE
+	-- Se excluirmos os dados de um destino, nao faz sentido manter os dados dos aeroportos nesse destino
 	
 )
 
 CREATE TABLE Voo (
 	
 	Nro INT,
-	Aeroporto_Origem INT	NOT NULL,
+	Aeroporto_Origem INT NOT NULL,
 	Aeroporto_Destino INT NOT NULL,
 	Data_Partida TIMESTAMP NOT NULL,
-	Data_Chegada TIMESTAMP NOT NULL,
+	Data_Chegada TIMESTAMP NOT NULL,-- Ambos sao TIMESTAMPS, para ja tratar o horario de um voo
 	Portao_Embarque VARCHAR(30) NOT NULL,
-	Portao_Desembarque VARCHAR(30) NOT NULL,
+	Portao_Desembarque VARCHAR(30) NOT NULL,-- Ambos são VARCHARs pois alguns aeroportos usam caracteres alfanumericos nos seus portoes
 	
 	CONSTRAINT PK_Voo PRIMARY KEY(Nro),
 	CONSTRAINT FK_VooOrigem FOREIGN KEY (Aeroporto_Origem) REFERENCES Aeroporto(ID) ON DELETE CASCADE ON UPDATE CASCADE,
+	-- A FK de origem deve ser deletada se o aeroporto for deletado, ja que um voo depende necessariamente do aeroporto que o planejou
 	CONSTRAINT FK_VooDestino FOREIGN KEY (Aeroporto_Destino) REFERENCES Aeroporto(ID) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT CK_Data CHECK (Data_Partida < Data_Chegada)
+    -- A FK de destino deve ser deletada se o aeroporto for deletado, ja que um voo depende necessariamente do aeroporto que o planejou
+	-- TODO Discutir se nao vale a pena deixar ON DELETE SET NULL, ja que um novo destino pode ser marcado
+	CONSTRAINT CK_Data CHECK (Data_Partida < Data_Chegada)
+	-- E impossivel a data de chegada ser antes da de partida
+	-- TODO Na verdade e possivel sim, se ele pegar um voo e for contra os fuso horarios. Discutir isso 
 	
+)
+
+CREATE TABLE VooAssentos (
+
+	Voo INT,
+	Assentos INT,
+	-- TODO O que significa Assentos? E um INT mesmo? N e composto isso aqui nao?
+
+	CONSTRAINT PK_VooAssentos PRIMARY KEY(Voo, Assentos),
+	CONSTRAINT FK_VooAssentos FOREIGN KEY(Voo) REFERENCES Voo(Nro) ON DELETE CASCADE ON UPDATE CASCADE,
+
 )
